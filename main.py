48b7b8b24e29f79fccb7bef8bc5c79fcb2cf3b44
martinspetlik <martin.spetlik>
2021-04-20 15:14:18 +0200

    quadratic

--- main.py
+++ main.py
@@ -17,12 +17,42 @@ def exact_solution_a(x, Q=1):
     return 1 - np.exp(Q*x)
 
 
-def conductivity_1(x, Q=1):
+def conductivity_a(x, Q=1):
     return np.exp(-Q*x)
 
 
-class Linear():
+def exact_solution_b(x, Q=1, L=1):
+    if isinstance(x, (list,np.ndarray)):
+        res = np.zeros(len(x))
+        print("x shape ", x.shape)
+        print("res shape ", res.shape)
+        res[x <= -L/3] = -Q*(1000*x[x <= -L/3] + 333*L)
+        res[x >= -L/3] = -Q * x[x >= -L/3]
+        return res
+    else:
+        if x <= -L/3:
+            return -Q*(1000*x + 333*L)
+        if x >= -L/3:
+            return -Q*x
 
+
+def conductivity_b(x, Q=1, L=1):
+    if isinstance(x, (list, np.ndarray)):
+        res = np.empty(len(x))
+        res[x <= -L/3] = 0.001
+        res[x >= -L/3] = 1
+        return res
+    else:
+        if x <= -L/3:
+            return 0.001
+        if x >= -L/3:
+            return 1
+
+# def conductivity_1_der(x, Q=1):
+#     return -Q*np.exp(-Q*x)
+
+
+class BasisFunctions:
     @staticmethod
     def points_from_ref_to_ele(x1, h, x):
         """
@@ -31,7 +61,7 @@ class Linear():
         :param x: reference element point(s)
         :return: element points
         """
-        return x1 + h*x
+        return x1 + h * x
 
     @staticmethod
     def points_from_ele_to_ref(x1, h, x):
@@ -41,8 +71,10 @@ class Linear():
         :param x: reference element point(s)
         :return: element points
         """
-        return (x-x1)/h
+        return (x - x1) / h
 
+
+class Linear(BasisFunctions):
     @staticmethod
     def phi_ref_1(x):
         return 1-x
@@ -60,14 +92,61 @@ class Linear():
     def phi_ref_2_der(x=None, h=1):
         return (1/h)
 
+    @staticmethod
+    def get_functions_der():
+        return [Linear.phi_ref_1_der, Linear.phi_ref_2_der]
 
-def FEM(N):
+    @staticmethod
+    def get_functions():
+        return [Linear.phi_ref_1, Linear.phi_ref_2]
+
+
+class Quadratic(BasisFunctions):
+    @staticmethod
+    def phi_ref_1(x):
+        return 2*(x**2) - 3*x +1
+
+    @staticmethod
+    def phi_ref_2(x):
+        return -4*(x**2) + 4*x
+
+    @staticmethod
+    def phi_ref_3(x):
+        return 2 * (x ** 2) - x
+
+    @staticmethod
+    def phi_ref_1_der(x=None, h=1):
+        return (4*x-3)*(1/h)
+
+    @staticmethod
+    def phi_ref_2_der(x=None, h=1):
+        return (-8 * x + 4) * (1/h)
+
+    @staticmethod
+    def phi_ref_3_der(x=None, h=1):
+        return (4*x-1)*(1/h)
+
+    @staticmethod
+    def get_functions_der():
+        return [Quadratic.phi_ref_1_der, Quadratic.phi_ref_2_der, Quadratic.phi_ref_3_der]
+
+    @staticmethod
+    def get_functions():
+        return [Quadratic.phi_ref_1, Quadratic.phi_ref_2, Quadratic.phi_ref_3]
+
+def FEM(N, quadratic=False):
+    #quadratic = True
+    exact_solution = exact_solution_b
+    conductivity = conductivity_b
     L = 1
     Q = 1
-    h = (0-(-L)) / (N-1)
+    N_nodes = N+1
+    h = (0-(-L)) / (N_nodes)
     print("h ", h)
-    x_ele = np.linspace(-L, 0, N, endpoint=True)
+    x_ele = np.linspace(-L, 0, N_nodes, endpoint=True)
     basis_func = Linear
+    if quadratic:
+        basis_func = Quadratic
 
     print("x_ele ", x_ele)
     ref_ele = [0, 1]
@@ -76,136 +155,133 @@ def FEM(N):
     pt = (pt + 1) / 2 * (ref_ele[1] - ref_ele[0]) + ref_ele[0]
     w = w * (ref_ele[1] - ref_ele[0]) / 2
 
-    #pt = 0.5 * (pt + 1) * (ref_ele[1] - ref_ele[0]) + ref_ele[0]  # rescale quandrature points to ref element [0, 1]
-
     print("pt ", pt)
     print("w ", w)
 
-    A = np.zeros((N, N))
-    b = np.zeros(N-1)
+    print("N ", N)
+    #exit()
 
+    if quadratic:
+        N_nodes = N_nodes + N_nodes-1
+    print("N ", N)
+    A = np.zeros((N_nodes, N_nodes))
+    b = np.zeros(N_nodes-1)
+
+    functions = basis_func.get_functions_der()
+
+    print("x ele ", x_ele)
+    z =0
     for k, x_k in enumerate(x_ele[:-1]):
-        print("x_k ", x_k)
+        #k = k
+        if quadratic:
+            z = 2*k
+        else:
+            z = k
+        print("K index ", k)
+        #print("x_k ", x_k)
         # the particular element is range [x_k, x_{k+1}]
-        ele_ref_x = Linear.points_from_ref_to_ele(x_k, h, pt)
-        print("ele ref x ", ele_ref_x)
+        #ele_ref_x = Linear.points_from_ref_to_ele(x_k, h, pt)
+        #print("ele ref x ", ele_ref_x)
 
-        phi_1, phi_2, phi_1_der, phi_2_der = Linear.phi_ref_1, Linear.phi_ref_2, Linear.phi_ref_1_der, Linear.phi_ref_2_der
+        M = np.zeros((len(functions), len(functions)))
+        for i in range(len(functions)):
+            for j in range(i, len(functions)):
+                for quad_pt, quad_w in zip(pt, w):
+                    x_c = basis_func.points_from_ref_to_ele(x_k, h, quad_pt)
+                    #print("x_k:{}, x_c:{}, quad_pt: {}".format(x_k, x_c, quad_pt))
 
-        M = np.zeros((2, 2))
+                    if k == len(x_ele) - 2:
+                        #print("LAST k: {}".format(k))
+                        #if i == 1 or j == 1:
+                        if i == len(functions)-1 or j == len(functions)-1:
+                            print("i: {}, j: {}".format(i, j))
+                            M[i,j] += 0
+                        else:
+                            M[i, j] += quad_w * conductivity(x_c, Q) * functions[i](quad_pt, h) * functions[j](quad_pt, h) * h
+                    else:
+                        M[i, j] += quad_w * conductivity(x_c, Q) * functions[i](quad_pt, h) * functions[j](quad_pt, h) * h
+                        M[j, i] = M[i,j]
 
-                # def scipy_quad_1(x):
-                #     x_c = Linear.points_from_ref_to_ele(x_k, h, x)
-                #     return conductivity_1(x_c, Q) * phi_1_der(x, h) * h
-                #
-                # def scipy_quad(x):
-                #     x_c = Linear.points_from_ref_to_ele(x_k, h, x)
-                #     print("x: {}, x_c:{}".format(x, x_c))
-                #     return phi_1_der(x, h) * phi_2_der(x, h) * h * conductivity_1(x_c, Q)
-                #
-                # if k == len(x_ele) - 2:
-                #     M[i, j] = integrate.quad(scipy_quad_1, ref_ele[0], ref_ele[1])[0]
-                # else:
-                #     res = integrate.quad(scipy_quad, ref_ele[0], ref_ele[1])
-                #     print("res ", res)
-                #     M[i, j] = res[0]
+        k_index = z
+        A[k_index:(k_index+len(functions)), k_index:(k_index+len(functions))] += M
 
-        for quad_pt, quad_w in zip(pt, w):
-            x_c = Linear.points_from_ref_to_ele(x_k, h, quad_pt)
-            print("x_k:{}, x_c:{}, quad_pt: {}".format(x_k, x_c, quad_pt))
-            # for i in range(2):
-            #     for j in range(2):
-            if k == len(x_ele) - 2:
-                M[0, 0] += quad_w * conductivity_1(x_c, Q) * phi_1_der(quad_pt, h) * phi_1_der(quad_pt, h) * h
-                M[1, 0] += 0
-                M[0, 1] += 0
-                M[1, 1] += 0
-            else:
-                M[0, 0] += quad_w * conductivity_1(x_c, Q) * phi_1_der(quad_pt, h) * phi_1_der(quad_pt, h) * h
-                M[0, 1] += quad_w * conductivity_1(x_c, Q) * phi_1_der(quad_pt, h) * phi_2_der(quad_pt, h) * h
-                M[1, 0] += quad_w * conductivity_1(x_c, Q) * phi_2_der(quad_pt, h) * phi_1_der(quad_pt, h) * h
-                M[1, 1] += quad_w * conductivity_1(x_c, Q) * phi_2_der(quad_pt, h) * phi_2_der(quad_pt, h) * h
-
-        #M = M * 0.5 * (ref_ele[1] - ref_ele[0])
-        print("M ", M)
-        #M = M/h
-        A[k:(k+2), k:(k+2)] += M[:,:]
-        #b[k:(k+2)] += h
-
-    b[0] += Q
-
-    A = A[:-1, :-1]
+    b[0] = Q
+    if quadratic:
+        A = A[:-1, :-1]
+        b = b#[:-1]
+    else:
+        A = A[:-1, :-1]
 
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):
         print("A ")
         print(pd.DataFrame(A))
 
-    print("A[k:k+1, k:k+1] ", A[k:k + 1, k:k + 1])
-    print("b ", b)
-    print("A ", A)
-
+    # print("A[k:k+1, k:k+1] ", A[k:k + 1, k:k + 1])
+    # print("b ", b)
+    # print("A ", A)
 
     p_coefs = np.linalg.solve(A, b)
     print("p ", p_coefs)
 
 
-    x = np.linspace(-L, 0, 10)
-    x = x_ele
-    res = []
-    #print("x ", x)
-    for x_i in x:
-        s = 0
-        for k in range(N-1):
-            #print("k: {}, x: {}".format(k, x_i))
-            #x_ref = Linear.points_from_ele_to_ref(x_ele[k], h, x_i)
-            s += p_coefs[k] * linear_basis_functions(k, x_i, x_ele, h)
-        res.append(s)
+    x = np.linspace(-L, 0, 250)
+    functions = basis_func.get_functions()
+    resolution_per_ele = 10
+    ele_x_values = []
+    ele_y_values = []
 
-    print("res ", res)
+    for k, x_k in enumerate(x_ele[:-1]):
+        if quadratic:
+            k = k*2
+        #print("x_k ", x_k)
+        x_ref = np.linspace(0, 1, resolution_per_ele)
+        ele_x = basis_func.points_from_ref_to_ele(x_k, h, x_ref)
+        #print("element_x ", ele_x)
 
-    plt.plot(x, res)
+        ele_x_values.extend(ele_x)
+
+        s = np.zeros(resolution_per_ele)
+        for phi in functions:
+            s += p_coefs[k] * phi(x_ref)
+        ele_y_values.extend(s)
+
+    #plt.plot(x_ele, p_coefs, label="aprox")
+    plt.plot(ele_x_values, ele_y_values, label="aprox")
+    plt.plot(x, exact_solution(x), label="exact")
+    plt.legend()
+    plt.yscale('log')
     plt.show()
 
-    # for e in (elements):
-    #     # prevest na referencni element
-    #     for quad:
-    #         for i:
-    #             for j:
-    #                 # pridani prvku do lokalni matice M
-    #
-    #      A += M
-    #
+
+# def linear_basis_functions(i, x, x_h, h):
+#
+#     if i == 0:
+#         if x < x_h[i]:
+#             return 0
+#         elif x_h[i] <= x < x_h[i + 1]:
+#             return 1 - (x - x_h[i]) / h
+#         elif x >= x_h[i + 1]:
+#             return 0
+#     else:
+#         if x < x_h[i-1]:
+#             return 0
+#         elif x_h[i-1] <= x < x_h[i]:
+#             return (x - x_h[i-1])/h
+#         elif x_h[i] <= x < x_h[i+1]:
+#             return 1 - (x-x_h[i])/h
+#         elif x >= x_h[i+1]:
+#             return 0
+#
+#     print("i ", i)
+#     print("x_h ", x_h)
+#     print("x: {}, x_h[i]: {}, x_h[i-1]: {}".format(x, x_h[i], x_h[i - 1]))
 
 
-def linear_basis_functions(i, x, x_h, h):
-
-    if i == 0:
-        if x < x_h[i]:
-            return 0
-        elif x_h[i] <= x < x_h[i + 1]:
-            return 1 - (x - x_h[i]) / h
-        elif x >= x_h[i + 1]:
-            return 0
-    else:
-        if x < x_h[i-1]:
-            return 0
-        elif x_h[i-1] <= x < x_h[i]:
-            return (x - x_h[i-1])/h
-        elif x_h[i] <= x < x_h[i+1]:
-            return 1 - (x-x_h[i])/h
-        elif x >= x_h[i+1]:
-            return 0
-
-    print("i ", i)
-    print("x_h ", x_h)
-    print("x: {}, x_h[i]: {}, x_h[i-1]: {}".format(x, x_h[i], x_h[i - 1]))
-
-
-def exact_solution_b(x, Q=1, L=1):
-    #@TODO: which value should be set in -L/3?
-    if x <= -L/3:
-        return -Q*(1000*x + 333*L)
-    return -Q*x
+# def exact_solution_b(x, Q=1, L=1):
+#     #@TODO: which value should be set in -L/3?
+#     if x <= -L/3:
+#         return -Q*(1000*x + 333*L)
+#     return -Q*x
 
 
 def plot_exact_solution():
@@ -251,6 +327,17 @@ def local_matrix(N):
         #A[k:k+1, k:k+1] = M
 
 
+def plot_quadratic_basis_fce():
+    x = np.linspace(0, 1, 200)
+
+    plt.plot(x, Quadratic.phi_ref_1(x), label="ref 1")
+    plt.plot(x, Quadratic.phi_ref_2(x), label="ref 2")
+    plt.plot(x, Quadratic.phi_ref_3(x), label="ref 3")
+    plt.legend()
+
+    plt.show()
+
+
 # def plot_reference_basis_fce():
 #     N = 4
 #     L = 1
@@ -270,9 +357,12 @@ def local_matrix(N):
 
 
 if __name__ == '__main__':
-    N = 50
-    FEM(N)
+    N = 500# number of elements
+    # plot_quadratic_basis_fce()
+    # exit()
 
-    plot_exact_solution()
+    FEM(N, quadratic=True)
+
+    #plot_exact_solution()
     #plot_basis_functions(N)
     # plot_reference_basis_fce()
